c251e66debf20f3786bac126d85ce914d2bc92d4
brilianrn <brilian.rachmad13@gmail.com>
2021-04-12 17:12:31 +0700

    server done

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,8 +1,24 @@
 const { Admin } = require('../models');
+const { comparePass } = require('../helpers/passHelp');
 
 class AdminController{
   static login(req, res, next){
-    
+    const email = req.body.email;
+    const password = req.body.password;
+
+    Admin.findOne({ where: { email }})
+    .then(data => {
+      const cekPassword = comparePass(password, data.password);
+      if (cekPassword) {
+        res.status(200).json({ name: data.name, email: data.email })
+      } else {
+        throw new Error()
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      next(err);
+    })
   };
 };
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const route = express.Router();
 const userRoute = require('./userRoute');
-// const adminRoute = require('./adminRoute');
+const adminRoute = require('./adminRoute');
 
 route.use('/user', userRoute);
-// route.use('/admin', adminRoute);
+route.use('/admin', adminRoute);
 
 module.exports = route;
